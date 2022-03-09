e59b64d0d9b215403c708f5cd745b8417f94a3c6
Gah2k0 <gah.francisco@gmail.com>
2022-03-09 15:15:11 -0300

    Atualizado com innertext

--- aula-05/Mesa-Trabalho-aula05/main.js
+++ aula-05/Mesa-Trabalho-aula05/main.js
@@ -1,8 +1,16 @@
 let bodyReference = document.querySelector('body');
+let btn = document.querySelector('#btn')
 
 function changeMode() {
 
     bodyReference.classList.toggle("dark")
-    
+
+
+    if(bodyReference.classList.contains("dark")) {
+        btn.innerText = "Ativar Modo Light"
+    } else {
+        btn.innerText = "Ativar Modo Dark"
+    }
+
 }
 
